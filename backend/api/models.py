bb19f84056baa2e7593430884bb4191e55103650
PredictChain <your.email@example.com>
2025-05-08 00:32:03 +0100

    update models

--- backend/api/models.py
+++ backend/api/models.py
@@ -83,6 +83,7 @@ class Membership(models.Model):
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 
 class Event(models.Model):
     title = models.CharField(max_length=200)
